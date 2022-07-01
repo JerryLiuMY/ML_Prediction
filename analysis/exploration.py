c548b136ac0b8d9335bbd3e507ce57729f3ac401
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-06-30 19:54:20 -0500

    update

--- analysis/exploration.py
+++ analysis/exploration.py
@@ -1,9 +1,14 @@
 import os
 import glob
+import pickle
+import pandas as pd
 from global_settings import DATA_PATH
+from global_settings import LOG_PATH
 
 
 def check_overlap():
+    """Check for dates overlap in the X and y dataframes"""
+
     X_dates = []
     for file in glob.glob(os.path.join(DATA_PATH, "X", "*.pkl")):
         name = file.split("/")[-1].split(".")[0]
@@ -21,4 +26,30 @@ def check_overlap():
     X_unique = sorted(set(X_dates) - set(overlap))
     y_unique = sorted(set(y_dates) - set(overlap))
 
-    return overlap, X_unique, y_unique
+    with open(os.path.join(LOG_PATH, "overlap.pkl"), "wb") as f:
+        pickle.dump(overlap, f)
+
+    with open(os.path.join(LOG_PATH, "X_unique.pkl"), "wb") as f:
+        pickle.dump(X_unique, f)
+
+    with open(os.path.join(LOG_PATH, "y_unique.pkl"), "wb") as f:
+        pickle.dump(y_unique, f)
+
+
+def plot_cusip():
+    """Count for the number of unique cusip in the X and y dataframes"""
+
+    with open(os.path.join(LOG_PATH, "overlap.pkl"), "rb") as f:
+        overlap = pickle.load(f)
+
+    cusip_df = pd.DataFrame(columns=["X", "y", "common"], index=overlap)
+    for date in overlap:
+        X_cusip = list(pd.read_pickle(os.path.join(DATA_PATH, "X", f"{date}.pkl")).index)
+        y_cusip = list(pd.read_pickle(os.path.join(DATA_PATH, "y", f"{date}.pkl")).index)
+        common_cusip = list(set(X_cusip) & set(y_cusip))
+        cusip_df.loc[date, "X"] = len(X_cusip)
+        cusip_df.loc[date, "y"] = len(y_cusip)
+        cusip_df.loc[date, "common"] = len(common_cusip)
+    cusip_df.index.name = "date"
+    cusip_df.reset_index(drop=False, inplace=True)
+    cusip_df.to_csv(os.path.join(LOG_PATH, "cusip.csv"))
--- global_settings.py
+++ global_settings.py
@@ -5,6 +5,8 @@ from pathlib import Path
 # directories
 DESKTOP_PATH = str(Path(os.getcwd()).parent.absolute())
 DATA_PATH = os.path.join(DESKTOP_PATH, "shared")
+OUTPUT_PATH = os.path.join(DESKTOP_PATH, "result")
+LOG_PATH = os.path.join(OUTPUT_PATH, "log")
 
 
 # TODO
