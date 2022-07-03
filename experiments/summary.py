534dc48afcaa584166922b451930cd6a8097aa84
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-02 21:26:27 -0500

    Update summary.py

--- experiments/summary.py
+++ experiments/summary.py
@@ -16,11 +16,12 @@ def summarize(model_name, horizon, window):
     # find dates in the directory
     window_path = os.path.join(OUTPUT_PATH, model_name, f"horizon={horizon}", window["X"][0][0])
     file_names = [_.split("/")[-1] for _ in glob.glob(os.path.join(window_path, "predict", "*.pkl"))]
-    dates = [_.split(".")[0] for _ in file_names]
+    dates = sorted([_.split(".")[0] for _ in file_names])
 
     # build the summary dataframe
-    true_df = pd.DataFrame(index=cusip_sic["cusip"])
-    pred_df = pd.DataFrame(index=cusip_sic["cusip"])
+    industry = list(cusip_sic["sic"].apply(lambda _: int(str(_)[:2])))
+    true_df = pd.DataFrame(data={"industry": industry}, index=cusip_sic["cusip"])
+    pred_df = pd.DataFrame(data={"industry": industry}, index=cusip_sic["cusip"])
 
     for date in dates:
         true_sub_df = pd.read_pickle(os.path.join(DATA_PATH, "y", f"{date}.pkl"))
@@ -33,20 +34,34 @@ def summarize(model_name, horizon, window):
     # get cross-sectional correlation
     daily_corr = {}
     for date in dates:
-        daily_corr[date] = true_df[date].pred_df[date]
+        daily_corr[date] = true_df[date].corr(pred_df[date])
 
     # get time-series correlation
     cusip_corr = {}
     for cusip in cusip_sic["cusip"].values:
-        cusip_corr[cusip] = true_df.loc[cusip, :].corr(pred_df.loc[cusip, :])
+        true_df_row = true_df.loc[cusip, true_df.columns != "industry"]
+        pred_df_row = pred_df.loc[cusip, pred_df.columns != "industry"]
+        cusip_corr[cusip] = true_df_row.corr(pred_df_row)
 
-    # introduce industry
-    industry = list(cusip_sic["sic"].apply(lambda _: str(_)[:2]))
-    true_df["industry"] = industry
-    pred_df["industry"] = industry
+    # get correlation for each industry
+    daily_corr_ind = {}
+    cusip_corr_ind = {}
 
-    # get cross-sectional correlation for each industry
+    for ind in sorted(set(industry)):
+        daily_corr_temp = {}
+        cusip_corr_temp = {}
+        true_df_ind = true_df.loc[true_df["industry"] == ind]
+        pred_df_ind = pred_df.loc[pred_df["industry"] == ind]
 
-    # get time series correlation for each industry
+        for date in dates:
+            daily_corr_temp[date] = true_df_ind[date].corr(pred_df_ind[date])
 
-    pass
+        for cusip in true_df_ind.index.values:
+            true_df_ind_row = true_df_ind.loc[cusip, true_df.columns != "industry"]
+            pred_df_ind_row = pred_df_ind.loc[cusip, pred_df.columns != "industry"]
+            cusip_corr_temp[cusip] = true_df_ind_row.corr(pred_df_ind_row)
+
+        daily_corr_ind[str(ind)] = daily_corr_temp
+        cusip_corr_ind[str(ind)] = cusip_corr_temp
+
+    
