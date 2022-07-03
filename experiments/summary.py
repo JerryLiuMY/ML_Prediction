223bf2045d96b0855bdf7de7c5b351131fdfd5f0
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-02 20:39:46 -0500

    update

--- experiments/summary.py
+++ experiments/summary.py
@@ -1,3 +1,9 @@
+from global_settings import DATA_PATH
+from global_settings import OUTPUT_PATH
+from global_settings import cusip_sic
+import pandas as pd
+import glob
+import os
 
 
 def summarize(model_name, horizon, window):
@@ -6,6 +12,25 @@ def summarize(model_name, horizon, window):
     :param horizon: predictive horizon
     :param window: [trddt_train, trddt_valid, trddt_test] window
     """
+
+    # find dates in the directory
+    window_path = os.path.join(OUTPUT_PATH, model_name, f"horizon={horizon}", window["X"][0][0])
+    file_names = [_.split("/")[-1] for _ in glob.glob(os.path.join(window_path, "predict", "*.pkl"))]
+    dates = [_.split(".")[0] for _ in file_names]
+
+    # build the summary dataframe
+    true_df = pd.DataFrame(index=cusip_sic["cusip"])
+    pred_df = pd.DataFrame(index=cusip_sic["cusip"])
+
+    for date in dates:
+        true_sub_df = pd.read_pickle(os.path.join(DATA_PATH, "y", f"{date}.pkl"))
+        pred_sub_df = pd.read_pickle(os.path.join(window_path, "predict", f"{date}.pkl"))
+        true_sub_df = true_sub_df.rename(columns={"target": date})
+        pred_sub_df = pred_sub_df.rename(columns={"target": date})
+        true_df = true_df.join(true_sub_df, how="left")
+        pred_df = pred_df.join(pred_sub_df, how="left")
+
+    return true_df, pred_df
 
     # get cross-sectional correlation
 
--- global_settings.py
+++ global_settings.py
@@ -4,14 +4,21 @@ import pandas as pd
 from pathlib import Path
 
 
-# directories
+# define directories
 DESKTOP_PATH = str(Path(os.getcwd()).parent.absolute())
 DATA_PATH = os.path.join(DESKTOP_PATH, "shared")
 OUTPUT_PATH = os.path.join(DESKTOP_PATH, "result")
 TEMP_PATH = os.path.join(DESKTOP_PATH, "temp")
 LOG_PATH = os.path.join(OUTPUT_PATH, "log")
 
+# load necessary files
+# trddt_all: intersection between X and y
+# cusip_all: intersection between union(X) and union(y)
+# cusip_all: cusip_all with match to sic code
+
 trddt_all = np.asarray(pd.read_pickle(os.path.join(DATA_PATH, "trddt_all.pkl")))
+cusip_all = np.asarray(pd.read_pickle(os.path.join(DATA_PATH, "cusip_all.pkl")))
+cusip_sic = pd.read_csv(os.path.join(DATA_PATH, "cusip_sic.txt"), delim_whitespace=True)
 date0_min = "2010-01-04"
 date0_max = "2022-06-14"
 
