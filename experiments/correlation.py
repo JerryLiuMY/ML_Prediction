58740557ea9509c470f9d22e69341f050a6c3945
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-03 16:23:58 -0500

    Update correlation.py

--- experiments/correlation.py
+++ experiments/correlation.py
@@ -3,6 +3,7 @@ import numpy as np
 from global_settings import OUTPUT_PATH
 from global_settings import cusip_sic
 import matplotlib.pyplot as plt
+from params.params import window_dict
 import seaborn as sns
 import json
 import os
@@ -40,9 +41,9 @@ def build_correlation(model_name, horizon):
             pearson_corr.update(json.load(handle))
         with open(os.path.join(window_path, "summary", "spearman_corr.json"), "r") as handle:
             spearman_corr.update(json.load(handle))
-    pearson_corr_df = pd.DataFrame.from_dict(pearson_corr, columns=["pearson_corr"], orient="index")
-    spearman_corr_df = pd.DataFrame.from_dict(spearman_corr, columns=["spearman_corr"], orient="index")
-    corr_df = pd.concat([pearson_corr_df, spearman_corr_df])
+    pearson_df = pd.DataFrame.from_dict(pearson_corr, columns=["pearson"], orient="index")
+    spearman_df = pd.DataFrame.from_dict(spearman_corr, columns=["spearman"], orient="index")
+    corr_df = pd.concat([pearson_df, spearman_df], axis=1)
 
     return corr_ind_df, corr_df
 
@@ -56,13 +57,13 @@ def plot_correlation(corr_ind_df, corr_df, horizon_path):
     """
 
     # initialize correlation plot
-    fig = plt.figure(figsize=(15, 12))
+    fig = plt.figure(figsize=(18, 16))
     gs = fig.add_gridspec(8, 10)
-    ax = fig.add_subplot(gs[0:3, :])
-    ax1 = fig.add_subplot(gs[3:4, :8])
-    ax2 = fig.add_subplot(gs[3:4, :8])
-    ax3 = fig.add_subplot(gs[3:4, :8])
-    ax4 = fig.add_subplot(gs[3:4, :8])
+    ax = fig.add_subplot(gs[0:4, :])
+    ax1 = fig.add_subplot(gs[4:5, :8])
+    ax2 = fig.add_subplot(gs[5:6, :8])
+    ax3 = fig.add_subplot(gs[6:7, :8])
+    ax4 = fig.add_subplot(gs[7:8, :8])
 
     # ax: industrial correlation
     yticks = np.arange(len(corr_ind_df.index))
@@ -73,22 +74,29 @@ def plot_correlation(corr_ind_df, corr_df, horizon_path):
     ax.set_yticklabels(ylabels)
     ax.set_ylabel("SIC Industrial Code")
 
-    # ax1: pearson correlation
+    # define indices
     index = range(len(corr_df.index))
-    ax1.stem(index, corr_df["pearson_corr"].values, linefmt="#A9A9A9", markerfmt=" ", basefmt=" ")
-    ax1.scatter(index, corr_df["pearson_corr"].values, color="#899499", marker=".")
+    # test_size = window_dict["test_win"] - window_dict["valid_win"]
+    # xticks = [idx for idx, _ in enumerate(corr_df.index) if idx % test_size == 0]
+    # xlabels = [_ for idx, _ in enumerate(corr_df.index) if idx % test_size == 0]
+    # ax_.set_xticks(xticks)
+    # ax_.set_xticklabels(xlabels, rotation=30)
+
+    # ax1: pearson correlation
+    ax1.stem(index, corr_df["pearson"].values, linefmt="#A9A9A9", markerfmt=" ", basefmt=" ")
+    ax1.scatter(index, corr_df["pearson"].values, color="#899499", marker=".")
     ax1.get_xaxis().set_visible(False)
     ax1.set_ylabel("Pearson")
 
-    # ax2: cumulative correlation
+    # ax2: cumulative pearson correlation
 
     # ax3: spearman correlation
-    ax3.stem(index, corr_df["spearman_corr"].values, linefmt="#A9A9A9", markerfmt=" ", basefmt=" ")
-    ax3.scatter(index, corr_df["spearman_corr"].values, color="#899499", marker=".")
+    ax3.stem(index, corr_df["spearman"].values, linefmt="#A9A9A9", markerfmt=" ", basefmt=" ")
+    ax3.scatter(index, corr_df["spearman"].values, color="#899499", marker=".")
     ax3.get_xaxis().set_visible(False)
     ax3.set_ylabel("Spearman")
 
-    # ax4: cumulative rank correlation
+    # ax4: cumulative spearman correlation
 
     # save plotted figure
     plt.tight_layout()
