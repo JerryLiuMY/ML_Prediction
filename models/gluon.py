f2189d8366da5f2e5474c3dfda5b640e6d000611
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-08 20:01:10 -0500

    update

--- models/gluon.py
+++ models/gluon.py
@@ -1,6 +1,6 @@
-import os
-import pandas as pd
 from autogluon.tabular import TabularPredictor
+import pandas as pd
+import os
 
 
 def fit_autogluon(train_data, valid_data, params, window_path):
@@ -17,7 +17,7 @@ def fit_autogluon(train_data, valid_data, params, window_path):
     train_data.reset_index(drop=True, inplace=True)
     valid_data.reset_index(drop=True, inplace=True)
     predictor = TabularPredictor(label="target", path=os.path.join(window_path, "model"))
-    model = predictor.fit(train_data, tuning_data=valid_data, presets=presets,
+    model = predictor.fit(train_data, tuning_data=valid_data, presets=presets, excluded_model_types=["RF"],
                           ag_args_fit={"num_gpus": 1}, verbosity=2)
     model.save_space()
 
@@ -35,3 +35,14 @@ def pre_autogluon(model, test_data):
     target = pd.DataFrame(model.predict(test_data))
 
     return target
+
+
+# def get_leaderboard(model, test_data):
+#     """ Get the leaderboard of models in the ensemble
+#     :return: leaderboard
+#     """
+#
+#     model = TabularPredictor.load("model_path")
+#     leaderboard = model.leaderboard(test_data, silent=True)
+#
+#     return leaderboard
