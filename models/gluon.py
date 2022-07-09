ce0fabedfd06d6e180ef6c505764b2f7734f8784
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-09 01:02:59 -0500

    update

--- models/gluon.py
+++ models/gluon.py
@@ -17,7 +17,7 @@ def fit_autogluon(train_data, valid_data, params, window_path):
     train_data.reset_index(drop=True, inplace=True)
     valid_data.reset_index(drop=True, inplace=True)
     predictor = TabularPredictor(label="target", path=os.path.join(window_path, "model"))
-    model = predictor.fit(train_data, tuning_data=valid_data, presets=presets, excluded_model_types=["RF"],
+    model = predictor.fit(train_data, tuning_data=valid_data, presets=presets,
                           ag_args_fit={"num_gpus": 1}, verbosity=2)
     model.save_space()
 
