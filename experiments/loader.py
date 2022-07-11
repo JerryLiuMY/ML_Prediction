505ddb2a6e7282c42caa4bbaa5db645a26e6ee09
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-10 20:57:03 -0500

    update

--- experiments/loader.py
+++ experiments/loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from global_settings import DATA_PATH
+import pickle5 as pickle
 import os
 
 
@@ -24,8 +25,10 @@ def load_df(trddt_X, trddt_y):
 
     df = pd.DataFrame(columns=[f"x{num}" for num in range(798)] + ["target"])
     for t_X, t_y in zip(trddt_X, trddt_y):
-        X_sub = pd.read_pickle(os.path.join(DATA_PATH, "X", f"{t_X}.pkl"))
-        y_sub = pd.read_pickle(os.path.join(DATA_PATH, "y", f"{t_y}.pkl"))
+        with open(os.path.join(DATA_PATH, "X", f"{t_X}.pkl"), "rb") as handle:
+            X_sub = pickle.load(handle)
+        with open(os.path.join(DATA_PATH, "y", f"{t_y}.pkl"), "rb") as handle:
+            y_sub = pickle.load(handle)
         df_sub = pd.concat([X_sub, y_sub], axis=1, join="inner")
         df = pd.concat([df, df_sub], axis=0)
 
