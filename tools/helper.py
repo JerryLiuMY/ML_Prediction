b3cb459356af11f803227266d8057784f9da4fc5
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-10 21:34:16 -0500

    update

--- tools/helper.py
+++ tools/helper.py
@@ -2,17 +2,20 @@ import psutil
 import torch
 import nvsmi
 
-
 # RCC
 # ssh midway2.rcc.uchicago.edu -l mingyuliu -p 22
-# module load python
-# module load cuda/11.2
+# tmux attach -t ml_prediction
 # sinteractive --partition=gpu2 --gres=gpu:1 --time=36:00:00
+# module load python/anaconda-2020.02
+# module load cuda/11.2
+# conda activate base
+# cd /project2/dachxiu/mingyuliu
 
 # Risklab
 # ssh risklab.chicagobooth.edu -l mingyuliu -p 22
 # tmux attach -t ml_prediction
 # conda activate ml_prediction
+# cd /project2/mingyuliu
 
 # check CUDA
 print(f"CUDA version: {torch.version.cuda}")
@@ -27,3 +30,22 @@ print(f"Mem usage: {psutil.virtual_memory().percent}")
 # check GPU usage
 print(list(nvsmi.get_gpus())[0])
 print(list(nvsmi.get_gpus())[1])
+
+# RCC setup
+# ssh midway2.rcc.uchicago.edu -l mingyuliu -p 22
+# tmux attach -t ml_prediction
+# module load python
+# module load cuda/11.2
+# conda activate base
+# pip install -r requirements.txt
+# pip install "mxnet_cu101<2.0.0, >=1.7.0"
+# pip install lightgbm --install=--gpu
+
+# Risklab setup
+# ssh risklab.chicagobooth.edu -l mingyuliu -p 22
+# tmux attach -t ml_prediction
+# conda activate ml_prediction
+# conda install -c anaconda cudatoolkit=10.2
+# pip install -r requirements.txt
+# pip install "mxnet_cu101<2.0.0, >=1.7.0"
+# pip install lightgbm --install=--gpu
