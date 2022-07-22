1c6e7a925db79f99f4a0ff649051596275ef153f
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-21 22:29:43 -0500

    update

--- data_loader/loader.py
+++ data_loader/loader.py
@@ -1,7 +1,11 @@
+import numpy as np
 import pandas as pd
+import torch
+
 from global_settings import DATA_PATH
 import pickle5 as pickle
 import os
+from models.trans import seq_len, device
 
 
 def load_data(trddt_X, trddt_y, data_type):
@@ -33,3 +37,46 @@ def load_df(trddt_X, trddt_y):
         data_df = pd.concat([data_df, data_df_sub], axis=0)
 
     return data_df
+
+
+def get_data():
+    # construct a littel toy dataset
+    time = np.arange(0, 400, 0.1)
+    amplitude = np.sin(time) + np.sin(time * 0.05) + np.sin(time * 0.12) * np.random.normal(-0.2, 0.2, len(time))
+
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    amplitude = scaler.fit_transform(amplitude.reshape(-1, 1)).reshape(-1)
+
+    sampels = 2600
+    train_data = amplitude[:sampels]
+    test_data = amplitude[sampels:]
+
+    # convert our train data into a pytorch train tensor
+    train_sequence = create_inout_sequences(train_data, seq_len)
+    train_sequence = train_sequence[:-1]
+
+    test_data = create_inout_sequences(test_data, seq_len)
+    test_data = test_data[:-1]
+
+    return train_sequence.to(device), test_data.to(device)
+
+
+def create_inout_sequences(input_data, tw):
+    inout_seq = []
+    L = len(input_data)
+    for i in range(L - tw):
+        train_seq = input_data[i:i + tw]
+        train_label = input_data[i + 1:i + tw + 1]
+        inout_seq.append((train_seq, train_label))
+
+    return torch.FloatTensor(inout_seq)
+
+
+def get_batch(source, i, batch_size):
+    seq_len = min(batch_size, len(source) - 1 - i)
+    data = source[i:i + seq_len]
+    input = torch.stack(torch.stack([item[0] for item in data]).chunk(seq_len, 1))  # 1 is feature size
+    target = torch.stack(torch.stack([item[1] for item in data]).chunk(seq_len, 1))
+
+    return input, target
--- models/trans.py
+++ models/trans.py
@@ -1,9 +1,10 @@
-from matplotlib import pyplot
 import torch
 import torch.nn as nn
 import numpy as np
 import time
 import math
+from data_loader.loader import get_data, get_batch
+
 torch.manual_seed(0)
 np.random.seed(0)
 
@@ -16,8 +17,7 @@ np.random.seed(0)
 # tgt = torch.rand((20, 32, 512)) # (T,N,E)
 # out = transformer_model(src, tgt)
 
-input_window = 100  # number of input steps
-output_window = 1  # number of prediction steps, in this model its fixed to one
+seq_len = 100  # number of input steps
 batch_size = 10
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -53,15 +53,11 @@ def fit_transformer(train_data):
 # predict the next n steps based on the input data
 def pre_transformer(eval_model, data_source):
     eval_model.eval()
-    total_loss = 0.
-    test_result = torch.Tensor(0)
-    truth = torch.Tensor(0)
     data, _ = get_batch(data_source, 0, 1)
     with torch.no_grad():
-        output = eval_model(data[-input_window:])
-        data = torch.cat((data, output[-1:]))
+        target = eval_model(data[-seq_len:])
 
-    data = data.cpu().view(-1)
+    return target
 
 
 class PositionalEncoding(nn.Module):
@@ -83,8 +79,7 @@ class PositionalEncoding(nn.Module):
 class TransAm(nn.Module):
     def __init__(self, feature_size=250, num_layers=1, dropout=0.1):
         super(TransAm, self).__init__()
-        self.model_type = 'Transformer'
-
+        self.model_type = "Transformer"
         self.src_mask = None
         self.pos_encoder = PositionalEncoding(feature_size)
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=10, dropout=dropout)
@@ -104,7 +99,7 @@ class TransAm(nn.Module):
             self.src_mask = mask
 
         src = self.pos_encoder(src)
-        output = self.transformer_encoder(src, self.src_mask)  # , self.src_mask)
+        output = self.transformer_encoder(src, self.src_mask)
         output = self.decoder(output)
 
         return output
@@ -119,84 +114,6 @@ class TransAm(nn.Module):
 # if window is 100 and prediction step is 1
 # in -> [0..99]
 # target -> [1..100]
-def create_inout_sequences(input_data, tw):
-    inout_seq = []
-    L = len(input_data)
-    for i in range(L - tw):
-        train_seq = input_data[i:i + tw]
-        train_label = input_data[i + output_window:i + tw + output_window]
-        inout_seq.append((train_seq, train_label))
-    return torch.FloatTensor(inout_seq)
-
-
-def get_data():
-    # construct a littel toy dataset
-    time = np.arange(0, 400, 0.1)
-    amplitude = np.sin(time) + np.sin(time * 0.05) + np.sin(time * 0.12) * np.random.normal(-0.2, 0.2, len(time))
-
-    from sklearn.preprocessing import MinMaxScaler
-
-    # loading weather data from a file
-    # from pandas import read_csv
-    # series = read_csv('daily-min-temperatures.csv', header=0, index_col=0, parse_dates=True, squeeze=True)
-
-    # looks like normalizing input values curtial for the model
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    # amplitude = scaler.fit_transform(series.to_numpy().reshape(-1, 1)).reshape(-1)
-    amplitude = scaler.fit_transform(amplitude.reshape(-1, 1)).reshape(-1)
-
-    sampels = 2600
-    train_data = amplitude[:sampels]
-    test_data = amplitude[sampels:]
-
-    # convert our train data into a pytorch train tensor
-    # train_tensor = torch.FloatTensor(train_data).view(-1)
-    train_sequence = create_inout_sequences(train_data, input_window)
-    train_sequence = train_sequence[:-output_window]
-
-    # test_data = torch.FloatTensor(test_data).view(-1)
-    test_data = create_inout_sequences(test_data, input_window)
-    test_data = test_data[:-output_window]
-
-    return train_sequence.to(device), test_data.to(device)
-
-
-def get_batch(source, i, batch_size):
-    seq_len = min(batch_size, len(source) - 1 - i)
-    data = source[i:i + seq_len]
-    input = torch.stack(torch.stack([item[0] for item in data]).chunk(input_window, 1))  # 1 is feature size
-    target = torch.stack(torch.stack([item[1] for item in data]).chunk(input_window, 1))
-
-    return input, target
-
-
-def plot_and_loss(eval_model, data_source, epoch):
-    eval_model.eval()
-    total_loss = 0.
-    test_result = torch.Tensor(0)
-    truth = torch.Tensor(0)
-    with torch.no_grad():
-        for i in range(0, len(data_source) - 1):
-            data, target = get_batch(data_source, i, 1)
-            output = eval_model(data)
-            total_loss += criterion(output, target).item()
-            test_result = torch.cat((test_result, output[-1].view(-1).cpu()), 0)
-            truth = torch.cat((truth, target[-1].view(-1).cpu()), 0)
-
-    return total_loss / i
-
-
-def evaluate(eval_model, data_source):
-    eval_model.eval()  # Turn on the evaluation mode
-    total_loss = 0.
-    eval_batch_size = 1000
-    with torch.no_grad():
-        for i in range(0, len(data_source) - 1, eval_batch_size):
-            data, targets = get_batch(data_source, i, eval_batch_size)
-            output = eval_model(data)
-            total_loss += len(data[0]) * criterion(output, targets).cpu().item()
-
-    return total_loss / len(data_source)
 
 
 train_data, val_data = get_data()
@@ -215,18 +132,6 @@ for epoch in range(1, epochs + 1):
     epoch_start_time = time.time()
     fit_transformer(train_data)
 
-    if (epoch % 10 is 0):
-        val_loss = plot_and_loss(model, val_data, epoch)
-        pre_transformer(model, val_data)
-    else:
-        val_loss = evaluate(model, val_data)
-
-    print('-' * 89)
-    print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.5f} | valid ppl {:8.2f}'.format(epoch, (
-                time.time() - epoch_start_time), val_loss, math.exp(val_loss))
-          )
-    print('-' * 89)
-
     # if val_loss < best_val_loss:
     #    best_val_loss = val_loss
     #    best_model = model
@@ -235,6 +140,3 @@ for epoch in range(1, epochs + 1):
 
 # src = torch.rand(input_window, batch_size, 1) # (source sequence length,batch size,feature number)
 # out = model(src)
-#
-# print(out)
-# print(out.shape)
