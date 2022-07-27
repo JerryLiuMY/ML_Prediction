7d6c86e036202958f245aab62a323d39061a8312
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-27 00:48:12 -0500

    update

--- data_prep/data_split.py
+++ data_prep/data_split.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 from global_settings import DATA_PATH
 import pickle5 as pickle
+import pandas as pd
 import os
 
 
 def split_data():
     # load X/y and define indices
-    with open(os.path.join(DATA_PATH, "X.p"), "rb") as handle:
-        X = pickle.load(handle)
-
-    with open(os.path.join(DATA_PATH, "y.p"), "rb") as handle:
-        y = pickle.load(handle)
+    X = pd.read_pickle(os.path.join(DATA_PATH, "X.p"))
+    y = pd.read_pickle(os.path.join(DATA_PATH, "y.p"))
 
     # make directories
     X_path = os.path.join(DATA_PATH, "X")
@@ -35,10 +33,10 @@ def split_data():
         iloc = [i for i, _ in enumerate(X_index_0 == X_idx) if _]
         X_sub = X.iloc[iloc[0]: iloc[-1] + 1].droplevel("date")
         date = datetime.strftime(X_idx, "%Y-%m-%d")
-        X_sub.to_pickle(os.path.join(X_path, f"{date}.pkl"))
+        X_sub.to_pickle(os.path.join(X_path, f"{date}.pkl"), protocol=4)
 
     # split y
     for y_idx in y_index:
         y_sub = y.loc[y_idx]
         date = datetime.strftime(y_idx, "%Y-%m-%d")
-        y_sub.to_pickle(os.path.join(y_path, f"{date}.pkl"))
+        y_sub.to_pickle(os.path.join(y_path, f"{date}.pkl"), protocol=4)
--- experiments/loader.py
+++ experiments/loader.py
@@ -32,10 +32,8 @@ def load_df(trddt_X, trddt_y):
 
     data_df = pd.DataFrame(columns=[f"x{num}" for num in range(num_ft)] + ["target"])
     for t_X, t_y in zip(trddt_X, trddt_y):
-        with open(os.path.join(DATA_PATH, "X", f"{t_X[0]}.pkl"), "rb") as handle:
-            X_sub = pickle.load(handle)
-        with open(os.path.join(DATA_PATH, "y", f"{t_y}.pkl"), "rb") as handle:
-            y_sub = pickle.load(handle)
+        X_sub = pd.read_pickle(os.path.join(DATA_PATH, "X", f"{t_X[0]}.pkl"))
+        y_sub = pd.read_pickle(os.path.join(DATA_PATH, "y", f"{t_y}.pkl"))
         data_df_sub = pd.concat([X_sub, y_sub], axis=1, join="inner")
         data_df = pd.concat([data_df, data_df_sub], axis=0)
 
@@ -54,12 +52,10 @@ def load_dg(trddt_X, trddt_y):
     for t_X, t_y in zip(trddt_X, trddt_y):
         X_sub_li = []
         for _ in t_X:
-            with open(os.path.join(DATA_PATH, "X", f"{_}.pkl"), "rb") as handle:
-                X_sub = pickle.load(handle)
-                X_sub_li.append(X_sub)
+            X_sub = pd.read_pickle(os.path.join(DATA_PATH, "X", f"{_}.pkl"))
+            X_sub_li.append(X_sub)
 
-        with open(os.path.join(DATA_PATH, "y", f"{t_y}.pkl"), "rb") as handle:
-            y_sub = pickle.load(handle)
+        y_sub = pd.read_pickle(os.path.join(DATA_PATH, "y", f"{t_y}.pkl"))
 
         data_df_sub = pd.concat(X_sub_li, axis=1, join="inner")
         data_df_sub = pd.concat([data_df_sub, y_sub], axis=1, join="inner")
