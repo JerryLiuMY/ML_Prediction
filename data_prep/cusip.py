88cbd78cdd9d22af30756bc106f3347c953ec2b0
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-08-09 01:22:16 -0500

    Update cusip.py

--- data_prep/cusip.py
+++ data_prep/cusip.py
@@ -1,4 +1,5 @@
 from global_settings import DATA_PATH
+from tqdm.notebook import tqdm
 import pandas as pd
 import pickle5 as pickle
 import glob
@@ -12,7 +13,7 @@ def build_cusip():
     cusip_X = set()
     file_names = [_.split("/")[-1] for _ in glob.glob(os.path.join(DATA_PATH, "X", "*.pkl"))]
     file_names = sorted([_ for _ in file_names if "index" not in _])
-    for file_name in file_names:
+    for file_name in tqdm(file_names):
         X = pd.read_pickle(os.path.join(DATA_PATH, "X", file_name))
         cusip_X = cusip_X.union(set(X.index))
 
@@ -20,7 +21,7 @@ def build_cusip():
     cusip_y = set()
     file_names = [_.split("/")[-1] for _ in glob.glob(os.path.join(DATA_PATH, "y", "*.pkl"))]
     file_names = sorted([_ for _ in file_names if "index" not in _])
-    for file_name in file_names:
+    for file_name in tqdm(file_names):
         y = pd.read_pickle(os.path.join(DATA_PATH, "y", file_name))
         cusip_y = cusip_y.union(set(y.index))
 
